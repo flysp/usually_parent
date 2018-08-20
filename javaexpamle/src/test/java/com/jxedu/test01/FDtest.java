d2f74b6d6130b414b69cd8db1299f0c382ee4d86
libin <2724520350@qq.com>
2018-08-20 20:25:56 +0800

    commit 01

--- javaexpamle/src/test/java/com/jxedu/test01/FDtest.java
+++ javaexpamle/src/test/java/com/jxedu/test01/FDtest.java
@@ -4,19 +4,23 @@ import org.junit.Test;
 
 public class FDtest {
 
-      @Test
-     public void test01(){
+    @Test
+    public void test01() {
 
-           double a  = 12.30;
-           double b= 0.60;
+        double a = 12.30;
+        double b = 0.60;
 
-           double c  = a +b ;
+        double c = a + b;
 
-          System.out.println(c);
+        System.out.println(c);
 
-          System.out.println();
-      }
+        System.out.println();
+    }
 
+    @Test
+    public void test02() {
 
+        System.out.println("Git 测试02");
+    }
 
 }
